374057495c339f93c02c7eecf91dc5be2d5d0ab1
bfd <bendrikov.f@roistat.com>
2018-04-21 22:21:18 +0300

    dev

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
  * @return string
  */
 function server($uri) {
-    if (preg_match('/^\/test\/?&/', $uri)) {
+    if ($uri === '/') {
+        return '<h1>Main page</h1>';
+    }
+    if (preg_match('/^\/test\/?$/', $uri)) {
         return '<h1>TEST</h1>';
     }
+    return 'Not found';
 }
 
 $uri = $_SERVER['REQUEST_URI'];
